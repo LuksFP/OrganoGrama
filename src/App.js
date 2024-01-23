9a943255e7c896ffdc722332a0f2b7900fd5861c
Lucas Franco <lukaslzz79@gmail.com>
2024-01-23 14:01:44 -0300

    organo

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
+import Time from "./componentes/Time";
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+
+  const [times, setTimes] = useState([
+    {
+      id: uuidv4(),
+      nome: 'Orc',
+      cor: '#57C278'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Elfo',
+      cor: '#82CFFA'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Humano',
+      cor: '#A6D157'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Lizzard',
+      cor: '#E06B69'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Anão',
+      cor: '#DB6EBF'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Gigante',
+      cor: '#FFBA05'
+    },
+    {
+      id: uuidv4(),
+      nome: 'Goblin',
+      cor: '#FF8A29'
+    },
+  ]);
+
+  const inicial = [
+   
+    
+  ]
+
+  const [colaboradores, setColaboradores] = useState(inicial)
+
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
+  }
+
+  function mudarCor(cor, id) {
+    setTimes(times.map(time => {
+      if(time.id === id) {
+        time.cor = cor;
+      }
+      return time;
+    }));
+  }
+
+  function cadastrarTime({ nome, cor }) {
+    setTimes([...times, { nome, cor, id: uuidv4() }])
+  }
+
+  function resolverFavorito(id) {
+    setColaboradores(colaboradores.map(colaborador => {
+      if(colaborador.id === id) colaborador.favorito = !colaborador.favorito;
+      return colaborador;
+    }))
+  }
+
+
+  return (
+    <div>
+      <Banner />
+      <Formulario aoCriarTime={cadastrarTime} times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <section className="times">
+        <h1>Chars Criados</h1>
+        {times.map((time, indice) =>
+         <Time mudarCor={mudarCor}
+          key={indice} time={time} 
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={deletarColaborador} aoFavoritar={resolverFavorito} />)}
+      </section>
+      <Rodape />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import Campo from '../Campo'
+import ListaSuspensa from '../ListaSuspensa'
+import './formulario.css'
+
+const Formulario = ({ aoCadastrar, times, aoCriarTime }) => {
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+    const [habilidade, setHabilidade] = useState('')
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
+
+    const aoSubmeter = (evento) => {
+        evento.preventDefault()
+        console.log('form enviado', nome, cargo, imagem, time, habilidade )
+        aoCadastrar({
+            nome,
+            cargo,
+            imagem,
+            time,
+            habilidade
+        })
+    }
+
+    return (
+        <section className="formulario-container">
+            <form className="formulario" onSubmit={aoSubmeter}>
+                <h2>Preencha os dados para criar o card do seu Char.</h2>
+                <Campo
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite seu nome '
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}/>
+                <Campo
+                    obrigatorio={true}
+                    label='Classe' 
+                    placeholder='Digite sua Classe '
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}/>
+                <Campo 
+                    label='Imagem' 
+                    placeholder='Informe o endereço da imagem '
+                    aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa 
+                    obrigatorio={true}
+                    label='Raça'
+                    items={times} 
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}/>
+                    <Campo
+                     obrigatorio={true}
+                     label='Hablidade'
+                     placeholder='Digite Sua Habilidade '
+                     valor={habilidade}
+                     aoAlterado={valor => setHabilidade(valor)}/>
+                <Botao texto='Criar card' />
+            </form>
+            <form className="formulario" onSubmit={(evento) => {
+                evento.preventDefault()
+                aoCriarTime({ nome: nomeTime, cor: corTime })
+            }}>
+                <h2>Preencha os dados para criar uma nova raça.</h2>
+                <Campo
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite o nome da Raça'
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}/>
+                <Campo
+                    obrigatorio={true}
+                    type='color'
+                    label='Cor' 
+                    placeholder='Digite sua cor'
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}/>
+                <Botao texto='Criar Time' />
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
